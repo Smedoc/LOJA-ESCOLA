1ed191db022fdd244b558db898d36ec7f3c6f8f3
Smedoc <smedocussehi@gmail.com>
2023-01-25 21:19:38 +0100

    first commit

---
+++ admin/index.php
@@ -0,0 +1,157 @@
+<?php
+
+include '../components/connect.php';
+
+session_start();
+
+$admin_id = $_SESSION['admin_id'];
+
+if(!isset($admin_id)){
+   header('location:admin_login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Painel</title>
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <link rel="stylesheet" href="../css/admin_style.css">
+
+</head>
+<body>
+
+<?php include '../components/admin_header.php'; ?>
+
+<section class="dashboard">
+
+   <h1 class="heading">Painel Principal</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>Bem-vindo!</h3>
+         <p><?= $fetch_profile['name']; ?></p>
+         <a href="update_profile.php" class="btn">Actualizar perfil</a>
+      </div>
+
+      <div class="box">
+         <?php
+            $total_pendings = 0;
+            $select_pendings = $conn->prepare("SELECT * FROM `orders` WHERE payment_status = ?");
+            $select_pendings->execute(['pending']);
+            if($select_pendings->rowCount() > 0){
+               while($fetch_pendings = $select_pendings->fetch(PDO::FETCH_ASSOC)){
+                  $total_pendings += $fetch_pendings['total_price'];
+               }
+            }
+         ?>
+         <h3><span>$</span><?= $total_pendings; ?><span>/-</span></h3>
+         <p>Pedidos pendentes</p>
+         <a href="placed_orders.php" class="btn">Pedidos</a>
+      </div>
+
+      <div class="box">
+         <?php
+            $total_completes = 0;
+            $select_completes = $conn->prepare("SELECT * FROM `orders` WHERE payment_status = ?");
+            $select_completes->execute(['completed']);
+            if($select_completes->rowCount() > 0){
+               while($fetch_completes = $select_completes->fetch(PDO::FETCH_ASSOC)){
+                  $total_completes += $fetch_completes['total_price'];
+               }
+            }
+         ?>
+         <h3><span>$</span><?= $total_completes; ?><span>/-</span></h3>
+         <p>Pedidos Completos</p>
+         <a href="placed_orders.php" class="btn">Pedidos</a>
+      </div>
+
+      <div class="box">
+         <?php
+            $select_orders = $conn->prepare("SELECT * FROM `orders`");
+            $select_orders->execute();
+            $number_of_orders = $select_orders->rowCount()
+         ?>
+         <h3><?= $number_of_orders; ?></h3>
+         <p>Total de pedidos</p>
+         <a href="placed_orders.php" class="btn">Pedidos</a>
+      </div>
+
+      <div class="box">
+         <?php
+            $select_products = $conn->prepare("SELECT * FROM `products`");
+            $select_products->execute();
+            $number_of_products = $select_products->rowCount()
+         ?>
+         <h3><?= $number_of_products; ?></h3>
+         <p>Adicionar prdutos</p>
+         <a href="products.php" class="btn">Adicionar</a>
+      </div>
+
+      <div class="box">
+         <?php
+            $select_users = $conn->prepare("SELECT * FROM `users`");
+            $select_users->execute();
+            $number_of_users = $select_users->rowCount()
+         ?>
+         <h3><?= $number_of_users; ?></h3>
+         <p>Ususarios Normal</p>
+         <a href="users_accounts.php" class="btn">Usuarios</a>
+      </div>
+
+      <div class="box">
+         <?php
+            $select_admins = $conn->prepare("SELECT * FROM `admins`");
+            $select_admins->execute();
+            $number_of_admins = $select_admins->rowCount()
+         ?>
+         <h3><?= $number_of_admins; ?></h3>
+         <p>Usuarios Admins</p>
+         <a href="admin_accounts.php" class="btn">Admins</a>
+      </div>
+
+
+
+      <div class="box">
+         <?php
+         
+            $select_vendas = 0;
+            $select_vendas = $conn->prepare("SELECT * FROM `orders` WHERE total_products = ?");
+            $select_orders->execute(['comhttp://localhost/loja/http://localhost/loja/pleted']);
+            $number_of_vendas = $select_orders->rowCount();
+            if($select_orders->rowCount() > 0){
+               while($fetch_completes = $select_completes->fetch(PDO::FETCH_ASSOC)){
+                  $total_completes += $fetch_completes['total_price'];
+               }
+            }
+         ?>
+         <h3><?= $number_of_vendas; ?></h3>
+         <p>Produtos vendidos</p>
+         <a href="vandas.php" class="btn">Pedidos</a>
+      </div>
+
+   </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="../js/admin_script.js"></script>
+   
+</body>
+</html>
---
+++ components/footer.php
@@ -0,0 +1,38 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Links Rápidos</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Loja</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contactos</a>
+      </div>
+
+      <div class="box">
+         <h3>Links extras</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Entrar</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Carrinho</a>
+      </div>
+
+      <div class="box">
+         <h3>Contate-Nos</h3>
+         <a href="tel:1234567890"><i class="fas fa-phone"></i> +244 949 141 070</a>
+         <a href="tel:11122233333"><i class="fas fa-phone"></i> +244 949 141 070</a>
+         <a href="smedocussehi@gmail.com"><i class="fas fa-envelope"></i> smedocussehi@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <h3>Siga-nos</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+         <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Smedo Cussehi</span> | todos os direitos reservados!</div>
+
+</footer>
